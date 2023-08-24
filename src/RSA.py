e9e55e5a0302a878d9fa02c791c278dcb414b46d
Mattéo Ahouanto <91013295+matmicbro@users.noreply.github.com>
2023-08-24 10:14:50 +0200

    Added RSA Fermat Attack

--- src/RSA.py
+++ src/RSA.py
@@ -1,5 +1,5 @@
 from Crypto.Util.number import inverse, bytes_to_long, long_to_bytes, isPrime
-
+import math
 
 def test_parameters():
     print()
@@ -31,10 +31,13 @@ def check_params(N, e):
             inv = inverse(e, N-1)
             print(f"Euler Totient = {N-1}\nPrivate key = {inv}")
         except ValueError:
-            print("In fact, your exponent is not even invertible with your Modulus\n")
+            print("In fact, your exponent is not even invertible in the given Modulus\n")
         return
     print("Testing custom Wiener attack...")
     if wiener_attack(N,e):
+        return
+    print("Testing Fermat Factorization...")
+    if fermat(N, e):
         return
     print()
     print("Your parameters were strong enough to pass CryptoResolve!\nBut always be careful when using RSA!")
@@ -87,3 +90,43 @@ def wiener_attack(N,e):
         print(f"Wiener attack was successful because your private key is too small\nPrivate key = {privatekey}")
         return True
     return False
+
+
+def fermat(N, e):
+    def is_perfect_square(n):
+        """
+         quick test to know if an integer n is a perfect square
+        """
+        endings = ["00", "21", "41", "64", "89", "01", "24", "44", "69", "96", "04", "25", "49", "76", "09", "29", "56",
+                   "81", "16", "36", "61", "84"]
+        if str(n)[-2:] in endings:
+            return True
+        return False
+
+    def fermat_factorization(N):
+        """
+         N : an odd integer
+         returns:  two integers c, d such as N = cd
+        """
+        a = math.isqrt(N)
+        b_square = a * a - N
+        while not is_perfect_square(b_square):
+            a += 1
+            b_square = a * a - N
+        b = math.isqrt(b_square)
+        return a + b, a - b
+
+    p,q = fermat_factorization(N)
+    if p*q == N:
+        print()
+        print("Fermat Factorization attack was successful because\nthe factors of your Modulus were too close from eachothers")
+        phi = (p-1)*(q-1)
+        try:
+            d = inverse(e, phi)
+            print(f"Euler totient = {phi}\nPrivate key = {d}\n")
+        except ValueError:
+            print(f"Euler totient = {phi}")
+            print("Also found that your exponent was not invertible in the given modulus\n")
+        return True
+    return False
+
